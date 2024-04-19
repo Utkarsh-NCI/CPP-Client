e659bc170f8f7625c33009f9e12a104857706675
Utkarsh Singh <reachutkarshsingh@gmail.com>
2024-04-19 20:09:39 +0100

    Added alert

--- client/app/(dashboard)/upload/page.tsx
+++ client/app/(dashboard)/upload/page.tsx
@@ -1,17 +1,36 @@
 "use client";
-import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { ChangeEvent, useState } from "react";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { Button } from "@/components/ui/button";
+import { log } from "console";
 
 export default function Page() {
+  const [file, setFile] = useState<File | null>(null);
+  const [showAlert, setShowAlert] = useState(false);
+  const onFileChange = (event: ChangeEvent<HTMLInputElement>) => {
+    let _file: File | null = null;
+    if (event.target.files) {
+      _file = event.target.files[0];
+    }
+    setFile(_file);
+    setShowAlert(false);
+  };
+
+  // Divide file into small parts
+  const uploadFile = () => {
+    if (!file) {
+      setShowAlert(true);
+      return;
+    }
+    console.log(file);
+    const { size, type, name } = file;
+    const chunkSize = 256 * 1024; // 256 KB chunks
+    const maxConcurrentRequest = 5;
+  };
   return (
     <div className="flex flex-1 h-full justify-center items-center">
-      <div
-        className="border-4 border-dashed w-3/4 justify-center flex flex-col items-center h-2/3 space-y-1"
-        onDrop={(e) => {
-          e.preventDefault();
-          console.log("drop");
-        }}
-      >
+      <div className="border-4 border-dashed w-3/4 justify-center flex flex-col items-center h-2/3 space-y-1">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
@@ -28,7 +47,17 @@ export default function Page() {
         </svg>
         <p className="text-sm text-muted-foreground">Drag and Drop here</p>
         <p className="text-sm text-muted-foreground">or</p>
-        <Input type="file" className="max-w-md" />
+        <div className="flex space-x-4">
+          <Input type="file" className="max-w-md " onChange={onFileChange} />
+          <Button onClick={uploadFile}>Upload</Button>
+        </div>
+
+        <div className={`flex w-3/4 ${showAlert ? "" : "invisible"}`}>
+          <Alert variant="destructive">
+            <AlertTitle>Error</AlertTitle>
+            <AlertDescription>Please select a file to upload.</AlertDescription>
+          </Alert>
+        </div>
       </div>
     </div>
   );
